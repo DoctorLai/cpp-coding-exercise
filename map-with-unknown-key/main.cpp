5a963727f3b6616d95e98bd166ce288bda11d010
Zhihua Lai <zhihualai@microsoft.com>
2026-01-14 11:10:12 +0000

    Continued work

--- map-with-unknown-key/main.cpp
+++ map-with-unknown-key/main.cpp
@@ -13,7 +13,7 @@ main()
     std::map<std::string, int> my_map;
     std::print("Accessing unknown key 'unknown_key' in std::map:\n");
     int value = my_map["unknown_key"];
-    std::print("Value: {}\n", value); // Should print 0, as default-constructed int is 0
+    std::print("Value: {}\n", value);                         // Should print 0, as default-constructed int is 0
     std::print("Map size after access: {}\n", my_map.size()); // Should print 1
     return 0;
 }
